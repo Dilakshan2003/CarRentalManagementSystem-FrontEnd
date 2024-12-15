4104a2eb3fca60ead2934d13f7bb67b1a47488c6
ut01569 <ut01569@UNICOMTIC60>
2024-12-15 11:59:14 +0530

    add bg video

--- src/app/Components/window/window.component.ts
+++ src/app/Components/window/window.component.ts
@@ -1,45 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-window',
   standalone: false,
-  
+
   templateUrl: './window.component.html',
   styleUrl: './window.component.css'
 })
-export class WindowComponent  {
-
-  
-
-  // @ViewChild('videoElement', { static: false }) videoElement!: ElementRef;
-
-  // ngAfterViewInit() {
-  //   const video = this.videoElement.nativeElement;
-  //   video.currentTime = 0; // Reset the video to the start
-  // }
+export class WindowComponent {
 
 
+  @ViewChild('videoElement',{ static: false }) videoElement!: ElementRef;
 
-   images: string[] = [
+  videoUrl = 'images/car-window.mp4';
+
+  ngAfterViewInit() {
+    const video: HTMLVideoElement = this.videoElement.nativeElement;
+    video.loop = true;
+    video.autoplay = true;
+    video.muted = true;
+    video.play().catch(error => {
+      console.error('Error playing video:', error);
+    });
+  }
+
+
+
+  images: string[] = [
     'url(/images/bugatti-sport-car-77.jpg)',
     'url(/images/slide-2.webp)',
     'url(/images/slider-3.jpg)'
- ];
-    currentIndex: number = 0;
+  ];
+  currentIndex: number = 0;
 
-   constructor() { }
+  constructor() { }
 
-    ngOnInit(): void {
-     this.startImageSlider();
-      }
-
-     startImageSlider() {
-     setInterval(() => {
-       this.currentIndex = (this.currentIndex + 1) % this.images.length;
-     }, 4000); 
+  ngOnInit(): void {
+    this.startImageSlider();
   }
 
-   get backgroundImage() {
-     return this.images[this.currentIndex];
-   }
- }
+  startImageSlider() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 4000);
+  }
+
+  get backgroundImage() {
+    return this.images[this.currentIndex];
+  }
+}
